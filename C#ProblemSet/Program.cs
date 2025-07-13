08b35718c124699d2ccaff2a9087ec4f3767daa1
Harrison Sayre <hsayre42@gmail.com>
2025-07-13 16:13:04 -0400

    Did TwoSum

--- C#ProblemSet/Program.cs
+++ C#ProblemSet/Program.cs
@@ -5,18 +5,31 @@ using C_ProblemSet.Solutions;
 Console.WriteLine("Here is the solution");
 
 //Contains Duplicate
-/*int[] nums = //[1, 2, 3, 1];
+//int[] nums = //[1, 2, 3, 1];
 
-            //[1, 2, 3, 4];
+//            //[1, 2, 3, 4];
 
-            [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+//            [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
 
-Console.WriteLine(ContainsDuplicateSolution.ContainsDuplicate(nums));
-*/
+//Console.WriteLine(ContainsDuplicateSolution.ContainsDuplicate(nums));
 
 //Valid Anagram
-string s = "anagram", t = "nagaram";
-//string s = "rat", t = "car";
-//string s = "rat", t = "tar";
+//string s = "anagram", t = "nagaram";
+////string s = "rat", t = "car";
+////string s = "rat", t = "tar";
 
-Console.WriteLine(ValidAnagramSolution.IsAnagram(s, t));
+//Console.WriteLine(ValidAnagramSolution.IsAnagram(s, t));
+
+
+//TwoSum
+int[] nums = [2, 7, 11, 15];
+int target = 9;
+
+//int[] nums = [3, 2, 4];
+//int target = 6;
+
+//int[] nums = [3, 3];
+//int target = 6;
+
+int[] solution = TwoSumSolution.TwoSum(nums, target);
+Console.WriteLine(solution[0]+" "+solution[1]);
---
+++ C#ProblemSet/Solutions/TwoSumSolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ProblemSet.Solutions;
+
+public class TwoSumSolution
+{
+    public static int[]? TwoSum(int[] nums, int target)
+    {
+        int[] results = new int[2];
+
+        Dictionary<int, int> differences = new Dictionary<int, int>();
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int difference = target -nums[i];
+
+            if (differences.ContainsKey(difference)){
+                results[0] = differences[difference];
+                results[1]= i;
+                return results; 
+            }
+            differences[nums[i]] = i;
+        }
+        return null;
+    }
+}
