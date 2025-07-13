0fe4561b4d6161a262da1a1123050090d8641e16
Harrison Sayre <hsayre42@gmail.com>
2025-07-13 14:47:20 -0400

    Add project files.

---
+++ C#ProblemSet/ContainsDuplicateSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace C_ProblemSet;
+
+public class ContainsDuplicateSolution
+{
+
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        Dictionary<int, int> duplicateMap = new Dictionary<int, int>();
+
+        foreach (int i in nums)
+        {
+            if (duplicateMap.ContainsKey(i))
+            {
+                return true;
+            }
+
+            duplicateMap.Add(i, duplicateMap.GetValueOrDefault(i)+1);
+        }
+        return false; 
+    }
+
+}
---
+++ C#ProblemSet/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-c
+// onsole-template for more information
+using C_ProblemSet;
+
+int[] nums = //[1, 2, 3, 1];
+
+            //[1, 2, 3, 4];
+
+            [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+
+Console.WriteLine(ContainsDuplicateSolution.ContainsDuplicate(nums));
