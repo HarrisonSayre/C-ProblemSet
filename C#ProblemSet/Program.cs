b4edb7b92f46b77825de755613a59257e292ad5b
Harrison Sayre <hsayre42@gmail.com>
2025-07-15 22:37:28 -0400

    Forgot to do longest consecutive sequence.

--- C#ProblemSet/Program.cs
+++ C#ProblemSet/Program.cs
@@ -73,6 +73,12 @@ Console.WriteLine("Here is the solution");
 //char[][] board = new char[][] {row1, row2, row3, row4, row5, row6, row7, row8, row9 };
 //Console.WriteLine(ValidSudokuSolution.IsValidSudoku(board));
 
+//Longest Consecutive sequence
+//int[] nums = { 100, 4, 200, 1, 3, 2 };
+////int[] nums = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
+////int[] nums = {1,0,1,2};
+//Console.WriteLine(LongestConsecutiveSequenceSolution.LongestConsecutive(nums));
+
 //Valid Palindrome 
 //string s = "A man, a plan, a canal: Panama";
 ////string s = "race a car";
---
+++ C#ProblemSet/Solutions/LongestConsecutiveSequenceSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ProblemSet.Solutions;
+
+
+public class LongestConsecutiveSequenceSolution
+{
+    public static int LongestConsecutive(int[] nums)
+    {
+        HashSet<int> digits = new HashSet<int>();
+        int longest = 0;
+
+        foreach (int num in nums)
+        {
+            digits.Add(num);
+        }
+
+        foreach (int num in digits)
+        {
+            if (!digits.Contains(num - 1))
+            {
+                int cur = 1;
+                while (digits.Contains(num + cur))
+                {
+                    cur++;
+                }
+                if (cur > longest)
+                {
+                    longest = cur;
+                }
+            }
+        }
+
+        return longest;
+    }
+
+}
