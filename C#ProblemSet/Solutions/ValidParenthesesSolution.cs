cecb4cee2f993d19e16bc3ce03f21a5e095702be
Harrison Sayre <hsayre42@gmail.com>
2025-08-14 17:41:43 -0400

    Redid Valid Parens twice from memory.

--- C#ProblemSet/Solutions/ValidParenthesesSolution.cs
+++ C#ProblemSet/Solutions/ValidParenthesesSolution.cs
@@ -8,6 +8,90 @@ namespace C_ProblemSet.Solutions;
 
 internal class ValidParenthesesSolution
 {
+    public static bool IsValidMemory1(string s)
+    {
+
+        Stack<char> parenStack = new Stack<char>();
+
+        foreach (char paren in s)
+        {
+            if (paren == ')')
+            {
+                if (parenStack.Count() == 0)
+                {
+                    return false;
+                }
+                if (parenStack.Peek() != '(')
+                {
+                    return false;
+                }
+                parenStack.Pop();
+            }
+            else if (paren == ']')
+            {
+                if (parenStack.Count() == 0)
+                {
+                    return false;
+                }
+                if (parenStack.Peek() != '[')
+                {
+                    return false;
+                }
+                parenStack.Pop();
+            }
+            else if (paren == '}')
+            {
+                if (parenStack.Count() == 0)
+                {
+                    return false;
+                }
+                if (parenStack.Peek() != '{')
+                {
+                    return false;
+                }
+                parenStack.Pop();
+            }
+            else
+            {
+                parenStack.Push(paren);
+            }
+        }
+        return (parenStack.Count() == 0);
+    }
+
+    public static bool IsValidMemory2(string s)
+    {
+        Stack<char> parens = new Stack<char>();
+
+        Dictionary<char, char> closers = new Dictionary<char, char>();
+        closers.Add(')', '(');
+        closers.Add('}', '{');
+        closers.Add(']', '[');
+
+        foreach (char p in s)
+        {
+            if (closers.ContainsKey(p))
+            {
+                if (parens.Count == 0)
+                {
+                    return false;
+                }
+                else if (closers[p] != parens.Peek())
+                {
+                    return false;
+                }
+                parens.Pop();
+            }
+            else
+            {
+                parens.Push(p);
+            }
+        }
+
+        return (parens.Count() == 0);
+
+    }
+
     public static bool IsValid(string s)
     {
         Stack<char> parens = new Stack<char>();
@@ -19,6 +103,10 @@ internal class ValidParenthesesSolution
             }
             if (p == ')' || p == '}' || p == ']')
             {
+                if (parens.Count == 0)
+                {
+                    return false;
+                }
                 char top = parens.Peek();
                 if(top == '(' && p == ')')
                 {
