fd90dff12b9504fe5dea6b46de56bff9b675d971
Harrison Sayre <hsayre42@gmail.com>
2025-08-16 16:50:43 -0400

    Did evaluate RPN from memory twice.

--- C#ProblemSet/Solutions/RPNSolution.cs
+++ C#ProblemSet/Solutions/RPNSolution.cs
@@ -8,6 +8,85 @@ namespace C_ProblemSet.Solutions;
 
 public class RPNSolution
 {
+    public int EvalRPNMem2(string[] tokens)
+    {
+        Stack<int> mathStack = new Stack<int>();
+
+        foreach (string val in tokens)
+        {
+            if (val == "+")
+            {
+                mathStack.Push(mathStack.Pop() + mathStack.Pop());
+            }
+            else if (val == "*")
+            {
+                mathStack.Push(mathStack.Pop() * mathStack.Pop());
+            }
+            else if (val == "-")
+            {
+                int subtractor = mathStack.Pop();
+                int subtractee = mathStack.Pop();
+                mathStack.Push(subtractee - subtractor);
+            }
+            else if (val == "/")
+            {
+                int divisor = mathStack.Pop();
+                int numerator = mathStack.Pop();
+                mathStack.Push(numerator / divisor);
+            }
+            else
+            {
+                mathStack.Push(int.Parse(val));
+            }
+        }
+
+        return mathStack.Peek();
+
+    }
+
+    public int EvalRPNNMem1(string[] tokens)
+    {
+        Stack<String> rpnStack = new Stack<String>();
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            if (tokens[i] == "+")
+            {
+                int firstNumber = Int32.Parse(rpnStack.Pop());
+                int secondNumber = Int32.Parse(rpnStack.Pop());
+                string result = (firstNumber + secondNumber).ToString();
+                rpnStack.Push(result);
+            }
+            else if (tokens[i] == "-")
+            {
+                int firstNumber = Int32.Parse(rpnStack.Pop());
+                int secondNumber = Int32.Parse(rpnStack.Pop());
+                string result = (secondNumber - firstNumber).ToString();
+                rpnStack.Push(result);
+            }
+            else if (tokens[i] == "*")
+            {
+                int firstNumber = Int32.Parse(rpnStack.Pop());
+                int secondNumber = Int32.Parse(rpnStack.Pop());
+                string result = (firstNumber * secondNumber).ToString();
+                rpnStack.Push(result);
+            }
+            else if (tokens[i] == "/")
+            {
+                int firstNumber = Int32.Parse(rpnStack.Pop());
+                int secondNumber = Int32.Parse(rpnStack.Pop());
+                string result = (secondNumber / firstNumber).ToString();
+                rpnStack.Push(result);
+            }
+            else
+            {
+                rpnStack.Push(tokens[i]);
+            }
+        }
+
+        return Int32.Parse(rpnStack.Peek());
+    }
+
     public static int EvalRPN(string[] tokens)
     {
         Stack<int> result = new Stack<int>();
