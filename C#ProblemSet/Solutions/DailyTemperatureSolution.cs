1f51952ca5e528c87e6df9ba4d489464d8629aae
Harrison Sayre <hsayre42@gmail.com>
2025-08-17 21:06:44 -0400

    Did Daily Temp from memory twice.

--- C#ProblemSet/Solutions/DailyTemperatureSolution.cs
+++ C#ProblemSet/Solutions/DailyTemperatureSolution.cs
@@ -8,6 +8,42 @@ namespace C_ProblemSet.Solutions;
 
 public class DailyTemperatureSolution
 {
+        public int[] DailyTemperaturesMem2(int[] temperatures) {
+        Stack<int[]> temperatureStack = new Stack<int[]>();
+        int[] tempAnswers = new int[temperatures.Length];
+
+
+        for(int i = 0; i< temperatures.Length; i++){
+            while(temperatureStack.Count() > 0 && temperatures[i] > temperatureStack.Peek()[0]){
+                int[] currentPair = temperatureStack.Pop();
+                tempAnswers[currentPair[1]] = i - currentPair[1];
+            }
+            int[] newPair = {temperatures[i], i} ;
+            temperatureStack.Push(newPair);
+        }
+        return tempAnswers;
+
+    }
+
+    public int[] DailyTemperaturesMem1(int[] temperatures)
+    {
+        Stack<int[]> tempStack = new Stack<int[]>();
+        int[] tempAnswers = new int[temperatures.Length];
+
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            while (tempStack.Count() > 0 && temperatures[i] > tempStack.Peek()[0])
+            {
+                int[] curTop = tempStack.Pop();
+                tempAnswers[curTop[1]] = i - curTop[1];
+            }
+            int[] newPair = { temperatures[i], i };
+            tempStack.Push(newPair);
+        }
+
+        return tempAnswers;
+    }
+
     public static int[] DailyTemperatures(int[] temperatures)
     {
         Stack<int> tempindex = new Stack<int>();
