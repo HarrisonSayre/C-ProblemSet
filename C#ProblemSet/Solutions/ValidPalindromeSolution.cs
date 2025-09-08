ba663814d4403935e796d4e86d045f3d5196a5d2
Harrison Sayre <hsayre42@gmail.com>
2025-09-08 15:29:44 -0400

    Did ValidPalinedrome from memory.

--- C#ProblemSet/Solutions/ValidPalindromeSolution.cs
+++ C#ProblemSet/Solutions/ValidPalindromeSolution.cs
@@ -8,6 +8,31 @@ namespace C_ProblemSet.Solutions;
 
 internal class ValidPalindromeSolution
 {
+    public bool IsPalindromeMem1(string s)
+    {
+        int l = 0;
+        int r = s.Length - 1;
+
+        while (l < r)
+        {
+            while (!Char.IsLetterOrDigit(s[l]) && l < r)
+            {
+                l++;
+            }
+            while (!Char.IsLetterOrDigit(s[r]) && l < r)
+            {
+                r--;
+            }
+            if (Char.ToLower(s[l]) != Char.ToLower(s[r]))
+            {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
     public static bool IsPalindrome(string s)
     {
         string lowerS = s.ToLower();
