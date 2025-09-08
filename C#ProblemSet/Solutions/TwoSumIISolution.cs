c8bdddab7aa0130e23048b4fa80d66162913feb7
Harrison Sayre <hsayre42@gmail.com>
2025-09-08 16:00:30 -0400

    Did sorted TwoSum from memoery.

--- C#ProblemSet/Solutions/TwoSumIISolution.cs
+++ C#ProblemSet/Solutions/TwoSumIISolution.cs
@@ -8,6 +8,36 @@ namespace C_ProblemSet.Solutions;
 
 internal class TwoSumIISolution
 {
+
+    public int[] TwoSumMem1(int[] numbers, int target)
+    {
+        int l = 0;
+        int r = numbers.Length - 1;
+        int[] answer = new int[2];
+
+        while (l < r)
+        {
+            int curSum = numbers[l] + numbers[r];
+            if (curSum == target)
+            {
+                answer[0] = l + 1;
+                answer[1] = r + 1;
+                return answer;
+            }
+            if (curSum < target)
+            {
+                l++;
+            }
+            if (curSum > target)
+            {
+                r--;
+            }
+        }
+
+        return null;
+
+    }
+
     public static int[] TwoSum(int[] numbers, int target)
     {
         int i = 0;
