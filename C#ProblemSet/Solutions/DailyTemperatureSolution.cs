eebdfa7d1a5e05f1cf7cc6062e60a441e680b8ca
Harrison Sayre <hsayre42@gmail.com>
2025-09-29 20:20:13 -0400

    Did final instance of temperature problem using stack from memory.

--- C#ProblemSet/Solutions/DailyTemperatureSolution.cs
+++ C#ProblemSet/Solutions/DailyTemperatureSolution.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace C_ProblemSet.Solutions;
+﻿namespace C_ProblemSet.Solutions;
 
 public class DailyTemperatureSolution
 {
