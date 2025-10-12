7b466ed5ffb62a18deb8920f1fa0273b25bb7cde
Harrison Sayre <hsayre42@gmail.com>
2025-10-12 13:22:36 -0400

    Did Koko eating bananas.

---
+++ C#ProblemSet/Solutions/KokoSolution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ProblemSet.Solutions;
+
+public  class KokoSolution
+{
+    public int MinEatingSpeed(int[] piles, int h)
+    {
+        int left = 1;
+        int right = piles.Max();
+        int answer = right;
+        //Console.WriteLine(right);
+
+        while (left <= right)
+        {
+            int k = (left + right) / 2;
+
+            //Console.WriteLine(k);
+
+            int spentTime = 0;
+
+            foreach (int bananas in piles)
+            {
+                spentTime += (int)Math.Ceiling((double)bananas / k);
+            }
+
+            Console.WriteLine(spentTime);
+
+            if (spentTime <= h && spentTime >= 0)
+            {
+                answer = k;
+                right = k - 1;
+            }
+            else
+            {
+                left = k + 1;
+            }
+
+        }
+
+        return answer;
+    }
+
+}
