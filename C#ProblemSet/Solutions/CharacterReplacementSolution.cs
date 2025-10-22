a5afcd1829ad9e3521bdbacd7b5060b2a563855e
Harrison Sayre <hsayre42@gmail.com>
2025-10-22 17:23:16 -0400

    Did character replacement from memory.

--- C#ProblemSet/Solutions/CharacterReplacementSolution.cs
+++ C#ProblemSet/Solutions/CharacterReplacementSolution.cs
@@ -4,6 +4,50 @@ namespace C_ProblemSet.Solutions;
 
 public class CharacterReplacementSolution
 {
+
+    public int CharacterReplacementMem1(string s, int k)
+    {
+
+        int longest = 0;
+        int mostCommon = 0;
+        int left = 0;
+
+        Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+        for (int right = 0; right < s.Length; right++)
+        {
+
+            if (charCounts.ContainsKey(s[right]))
+            {
+                charCounts[(s[right])] = charCounts[(s[right])] + 1;
+                //Console.WriteLine(charCounts[s[right]]);
+            }
+            else
+            {
+                charCounts[s[right]] = 1;
+            }
+
+            //if(mostCommon < charCounts[s[right]]){
+            //    mostCommon = charCounts[s[right]];
+            //}
+            mostCommon = Math.Max(mostCommon, charCounts[s[right]]);
+
+            while (((right - left + 1) - mostCommon > k))
+            {
+                charCounts[s[left]]--;
+                left++;
+            }
+
+            //if(longest < (right-left+1)){
+            //    longest = right-left+1;    
+            //}
+
+            longest = Math.Max(longest, (right - left + 1));
+        }
+
+        return longest;
+    }
+
     public static int CharacterReplacement(string s, int k)
     {
         Dictionary<char, int> charTotals = new Dictionary<char, int>();
