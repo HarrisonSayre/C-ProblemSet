cb14f4475c68f73dc1befed157be10368b89d62b
Harrison Sayre <hsayre42@gmail.com>
2025-11-03 16:25:51 -0500

    Did reverse linked list from memory, both iteratively and recursively.

--- C#ProblemSet/Solutions/ReverseLinkedListSolution.cs
+++ C#ProblemSet/Solutions/ReverseLinkedListSolution.cs
@@ -19,4 +19,37 @@ public class ReverseLinkedListSolution
 
         return curHead;
     }
+
+    public ListNode ReverseListMem1(ListNode head)
+    {
+
+        // ListNode prev = null;
+        // ListNode curr = head;
+
+        // while(curr != null){
+        //     ListNode temp = curr.next;        
+        //     curr.next = prev;
+        //     prev = curr;
+        //     curr = temp;
+        // }
+
+        // return prev; 
+
+        if (head == null)
+        {
+            return null;
+        }
+
+        ListNode currentHead = head;
+        if (head.next != null)
+        {
+            currentHead = ReverseList(head.next);
+            head.next.next = head;
+        }
+
+        head.next = null;
+        return currentHead;
+
+    }
+
 }
