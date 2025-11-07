4a246cfba0b2beb2d85ba6875b3706d7a1dfc85b
Harrison Sayre <hsayre42@gmail.com>
2025-11-07 17:39:48 -0500

    Did linked list cycle using fast and slow pointers.

---
+++ C#ProblemSet/Solutions/LinkedListCycleSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ProblemSet.Solutions;
+
+public class LinkedListCycleSolution
+{
+    public bool HasCycle(ListNode head)
+    {
+        ListNode i = head;
+        ListNode j = head;
+
+        while (j != null && j.next != null)
+        {
+            j = j.next.next;
+            i = i.next;
+            if (i.Equals(j))
+            {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+}
