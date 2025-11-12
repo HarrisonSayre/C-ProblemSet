6daf9189c514438820c46cde1feea41bdabaddc4
Harrison Sayre <hsayre42@gmail.com>
2025-11-11 20:23:18 -0500

    Reordered a singly linked list to alternate between first and last using fast and slow pointers.

---
+++ C#ProblemSet/Solutions/ReorderListSolutioncs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ProblemSet.Solutions;
+
+public class ReorderListSolutioncs
+{
+    public void ReorderList(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode former = head;
+        ListNode latter = slow.next;
+        ListNode lastOne = slow.next = null;
+
+        while (latter != null)
+        {
+            ListNode temp = latter.next;
+            latter.next = lastOne;
+            lastOne = latter;
+            latter = temp;
+        }
+        latter = lastOne;
+
+        while (latter != null)
+        {
+            ListNode tempF = former.next;
+            ListNode tempL = latter.next;
+            former.next = latter;
+            latter.next = tempF;
+            former = tempF;
+            latter = tempL;
+        }
+
+    }
+
+}
