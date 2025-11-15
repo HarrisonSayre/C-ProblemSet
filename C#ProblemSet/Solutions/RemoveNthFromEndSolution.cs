6656d92b56da073b0ce1127df838cf83f8cb84c4
Harrison Sayre <hsayre42@gmail.com>
2025-11-14 20:22:14 -0500

    Removed nth node from a list using double pointers and looping.

---
+++ C#ProblemSet/Solutions/RemoveNthFromEndSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ProblemSet.Solutions;
+
+public class RemoveNthFromEndSolution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+
+        int m = 0;
+        ListNode tempStart = new ListNode(0, head);
+        ListNode left = tempStart;
+        ListNode right = head;
+
+        while (m < n)
+        {
+            right = right.next;
+            m++;
+        }
+
+        while (right != null)
+        {
+            left = left.next;
+            right = right.next;
+        }
+
+        left.next = left.next.next;
+
+        return tempStart.next;
+
+
+    }
+}
