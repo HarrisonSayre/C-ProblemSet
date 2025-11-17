ae39625530009bb42f0306c09e2554b3e00f234c
Harrison Sayre <hsayre42@gmail.com>
2025-11-17 16:38:53 -0500

    Did a deep copy of a linked list with random pointers using a hash map.

---
+++ C#ProblemSet/Solutions/CopyListWithRandomPointersSolution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace C_ProblemSet.Solutions;
+
+public class CopyListWithRandomPointersSolution
+{
+    public class Node
+    {
+        public int val;
+        public Node next;
+        public Node random;
+
+        public Node(int _val)
+        {
+            val = _val;
+            next = null;
+            random = null;
+        }
+    }
+
+    public Node CopyRandomList(Node head)
+    {
+
+        Dictionary<Node, Node> oldListMap = new Dictionary<Node, Node>();
+
+        Node current = head;
+        while (current != null)
+        {
+            Node newNode = new Node(current.val);
+            oldListMap[current] = newNode;
+            current = current.next;
+        }
+
+        current = head;
+        while (current != null)
+        {
+            Console.WriteLine(current.val);
+            Node newNode = oldListMap[current];
+            if (current.next == null)
+            {
+                //Console.WriteLine("Huh?");
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = oldListMap[current.next];
+                //Console.WriteLine(newNode.next.val);
+            }
+            if (current.random == null)
+            {
+                newNode.random = null;
+            }
+            else
+            {
+                newNode.random = oldListMap[current.random];
+            }
+            //Console.WriteLine(current.val);
+            current = current.next;
+        }
+
+        if (head == null)
+        {
+            return null;
+        }
+        else
+        {
+            return oldListMap[head];
+        }
+    }
+}
