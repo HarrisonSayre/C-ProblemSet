b7476f9f6fabe3009ff9cdb46e6eb631bd4cf377
Harrison Sayre <hsayre42@gmail.com>
2025-11-17 21:05:52 -0500

    Added numbers represented by two linked lists and returned the sum as a new linked list.

---
+++ C#ProblemSet/Solutions/AddTwoNumbersSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ProblemSet.Solutions;
+
+public class AddTwoNumbersSolution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+
+        ListNode start = new ListNode();
+        ListNode current = start;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int firstVal = (l1 != null) ? l1.val : 0;
+            int secondVal = (l2 != null) ? l2.val : 0;
+
+            int sum = firstVal + secondVal + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            current.next = new ListNode(sum);
+            current = current.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+
+        }
+
+        return start.next;
+
+    }
+}
